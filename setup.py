b017c4eccff22740948a54d589a2dce1496d432d
yaput <anton@bluelogic.ae>
2019-07-24 15:47:39 +0400

    Change package name

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='bl-core',
+setup(name='bl_core',
       version='0.1',
       description='Blue Logic Core Bot Package',
       url='',
       author='Blue Logic',
       author_email='anton@bluelogic.ae',
       license='MIT',
-      packages=['bl-core'],
+      packages=['bl_core'],
       install_requires=[
           'rasa-core==0.14.4',
           'rasa-core-sdk==0.14.0',
