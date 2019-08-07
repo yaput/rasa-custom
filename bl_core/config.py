3e1d92e4e27d422238c8eab361c38a9dfcf31032
yaput <anton@bluelogic.ae>
2019-08-07 11:44:32 +0400

    For windows

--- bl_core/config.py
+++ bl_core/config.py
@@ -7,7 +7,7 @@ def load_config(file='./config/development.config.json'):
     """
     env = os.getenv("BLUELOGIC_ENV", "development")
     file = './config/%s.config.json' % env
-    with open(file, encoding='Latin-1', errors='ignore') as cfg:
+    with open(file, encoding='iso-8859-1', errors='ignore') as cfg:
         logging.info("Config Loaded from: %s" % file)
         config = json.load(cfg)
         return config
