78023dd1134196363d5dbc787689ed6108a3744a
yaput <anton@bluelogic.ae>
2019-08-19 12:00:09 +0400

    Fox

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bl_core',
-      version='0.5.2',
+      version='0.5.3',
       description='Blue Logic Core Bot Package',
       url='',
       author='Blue Logic',
