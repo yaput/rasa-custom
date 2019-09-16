3471bd6b222580a0b7cf88f6eeab5f62826c488d
yaput <anton@bluelogic.ae>
2019-09-16 16:07:02 +0400

    Rever

--- bl_core/connection_interface.py
+++ bl_core/connection_interface.py
@@ -63,7 +63,7 @@ def pause_bot():
 def liveperson():
     req_data = request.get_json()
     userID = req_data['userId']
-    dashlog.log(userID, intent_name='Human In The Loop', queryText=req_data['text'])
+    dashlog.log("outgoing", None, userID,queryText=req_data['text'],intent_name='Human In The Loop')
     if not isPause(userID):
         pause_user(userID)
     send_message(userID, req_data['text'])
@@ -125,14 +125,14 @@ def handle_websocket(websocket, lang):
                     
                     responses = loop.run_until_complete(agent.handle_message(msgRasa))
                     for response in responses:
-                        dashlog.log(response['recipient_id'], intent_name="", queryText=response)
+                        dashlog.log("outgoing", response,response['recipient_id'])
                         time.sleep(1)
                         websocket.send(json.dumps(send_typing()))
                         time.sleep(1.5)
                         parsed_message = parse_bot_response(response)
                         websocket.send(json.dumps(parsed_message))
                 else:
-                    dashlog.log(session_message, intent_name="Human In The Loop", queryText=text_message, agent=False)
+                    dashlog.log("incoming", None, session_message,queryText=text_message,intent_name='Human In The Loop')
         except WebSocketError as ex:
             print(ex)
 
