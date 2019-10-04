4fb720954c95dcbfbd9801bb608a9976bc30d112
Ajudev <ajudevnair@gmail.com>
2019-10-04 16:35:04 +0400

    Fixed errors which were caused when executing nlu_train.py

--- bl_core/nlu_train.py
+++ bl_core/nlu_train.py
@@ -1,17 +1,17 @@
-from rasa.nlu.training_data import load_data
-from rasa.nlu.model import Trainer
-from rasa.nlu import config
+from rasa_nlu.training_data import load_data
+from rasa_nlu.model import Trainer
+from rasa_nlu import config
 
 def train_nlu(name, lang="en",production_build=False):
-    model_name = "production"
-    if not production_build:
-        model_name = "latest"
     training_data = load_data('./data/'+name+'/nlu/'+lang+"/")
     trainer = Trainer(config.load("config.yml"))
     trainer.train(training_data)
-    trainer.persist('./models/nlu/'+lang+"/", fixed_model_name=model_name)
+    trainer.persist('./models/nlu/'+lang+"/")
 
 import sys
+data = "hello"
+lang = "en"
+
 if sys.argv[1] == '--name':
     data = str(sys.argv[2])
 
