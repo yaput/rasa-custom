4d538688fdfcc81e1396d310a0106ee7e2ba0fa7
Ajudev <ajudevnair@gmail.com>
2019-10-04 16:40:13 +0400

    Added fixed model name which was removed earlier

--- bl_core/nlu_train.py
+++ bl_core/nlu_train.py
@@ -3,10 +3,14 @@ from rasa_nlu.model import Trainer
 from rasa_nlu import config
 
 def train_nlu(name, lang="en",production_build=False):
+    model_name = "production"
+    if not production_build:
+        model_name = "latest"
+
     training_data = load_data('./data/'+name+'/nlu/'+lang+"/")
     trainer = Trainer(config.load("config.yml"))
     trainer.train(training_data)
-    trainer.persist('./models/nlu/'+lang+"/")
+    trainer.persist('./models/nlu/'+lang+"/", fixed_model_name=model_name)
 
 import sys
 data = "hello"
