ead12868fc3d351d070c85eb6c7ee6ab3e6fffbd
Rino Alfian <rino@bluelogic.ae>
2019-10-13 15:51:23 +0400

    athena news

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bl_core',
-      version='1.0.0',
+      version='1.3.7',
       url='',
       author='Blue Logic',
       author_email='anton@bluelogic.ae',
