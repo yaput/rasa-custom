1fd93c25728f4f003a4ad5f3de9993377b59b81f
yaput <anton@bluelogic.ae>
2019-10-27 15:41:57 +0400

    Format code

--- bl_core/nlg.py
+++ bl_core/nlg.py
@@ -1,7 +1,18 @@
-import json, random, requests, re
+import sys
+import os
+from .tracker import Tracker
+from waitress import serve
+from flask import Flask, Response, request
+from .config import load_config
+import json
+import random
+import requests
+import re
+
 
 class NLG():
     """Create A Response based on user template"""
+
     def __init__(self, config):
         self.TYPE_TEXT = "text"
         self.TYPE_API = "api"
@@ -23,22 +34,22 @@ class NLG():
 
     def get_query(self):
         return self.request_data['tracker']['latest_message']['text']
-    
+
     def get_intent(self):
         intent = self.request_data['tracker']['latest_message']['intent']
         if intent['confidence'] <= self.config['nlu']['threshold']:
             return "default_fallback"
         return self.request_data['tracker']['latest_message']['intent']['name']
-    
+
     def get_sender(self):
         return self.request_data['tracker']['sender_id']
-    
+
     def _get_template(self):
         return self.request_data['template']
-        
+
     def _is_components(self):
         return
-    
+
     def _is_texts(self, object):
         return
 
@@ -55,17 +66,17 @@ class NLG():
     def _get_elements(self, lang):
         return self._get_content(lang)['elements']
 
-    def _make_response(self,text="", buttons=[], image=None, attachments=None):
+    def _make_response(self, text="", buttons=[], image=None, attachments=None):
         response = {}
         if text != "":
             response["text"] = text
-        
+
         if len(buttons) > 0:
             response["buttons"] = buttons
-        
+
         if image != None:
             response["image"] = image
-        
+
         if attachments != None:
             response["attachment"] = attachments
 
@@ -75,10 +86,11 @@ class NLG():
     def _get_text(self, lang):
         texts = self._get_elements(lang)
         selected_text = ""
-        selected_text = texts[random.randint(0, len(texts)-1) if len(texts) > 1 else 0]
-    
+        selected_text = texts[random.randint(
+            0, len(texts)-1) if len(texts) > 1 else 0]
+
         return selected_text
-    
+
     def _get_slots(self):
         return self.request_data['tracker']['slots']
 
@@ -99,9 +111,11 @@ class NLG():
         text = template
         for m in match:
             if m not in slot.keys():
-                text = template.replace(m, "undefined").replace("{","").replace("}","")
+                text = template.replace(m, "undefined").replace(
+                    "{", "").replace("}", "")
             else:
-                text = template.replace(m, slot[m]).replace("{","").replace("}","")
+                text = template.replace(m, slot[m]).replace(
+                    "{", "").replace("}", "")
         return text
 
     def _generate_attachment(self, content):
@@ -113,14 +127,14 @@ class NLG():
                 data[key] = self._replace_template_with_value(value)
             if e['method'] == "POST":
                 if e['body']['type'] == "form":
-                    resp = requests.post(url,data=data, headers=e['headers'])
+                    resp = requests.post(url, data=data, headers=e['headers'])
                 else:
-                    resp = requests.post(url,json=data, headers=e['headers'])
+                    resp = requests.post(url, json=data, headers=e['headers'])
             else:
                 resp = requests.get(self._replace_template_with_value(url))
 
             data = json.loads(resp.text, format="utf-8")
-            
+
             # temp_element = []
             postbacks = []
             embed = ""
@@ -130,17 +144,17 @@ class NLG():
             if 'split_text' in e['output']['elements'].keys():
                 split_text = e['output']['elements']['split_text']
             if '$' in e['output']['elements']['value']:
-                val_lookup = e['output']['elements']['value'].replace("$","").split(".")
+                val_lookup = e['output']['elements']['value'].replace(
+                    "$", "").split(".")
                 if isinstance(data, list):
                     for d in data:
                         v = None
                         v_temp = d[val_lookup[0]]
                         for f in val_lookup[1:]:
                             v = v_temp[f]
-                        
 
                         if embed != "":
-                            v = embed.replace('@',v)
+                            v = embed.replace('@', v)
                         postbacks.append(v)
                 elif isinstance(data[val_lookup[0]], list):
                     for d in range(len(data[val_lookup[0]])):
@@ -148,25 +162,25 @@ class NLG():
                         v_temp = data[val_lookup[0]][d]
                         for f in val_lookup[1:]:
                             v = v_temp[f]
-                        
 
                         if embed != "":
-                            v = embed.replace('@',v)
+                            v = embed.replace('@', v)
                         postbacks.append(v)
                 else:
                     v = None
                     for f in val_lookup:
                         v = data[f]
-                
+
                     if embed != "":
-                        v = embed.replace('@',v)
+                        v = embed.replace('@', v)
                     postbacks.append(v)
             else:
                 postbacks.append(e['output']['elements']['value'])
 
             labels = []
             if '$' in e['output']['elements']['text']:
-                text_lookup = e['output']['elements']['text'].replace("$","").split(".")
+                text_lookup = e['output']['elements']['text'].replace(
+                    "$", "").split(".")
                 if isinstance(data, list):
                     for d in data:
                         t = None
@@ -187,14 +201,15 @@ class NLG():
                     t = None
                     for f in text_lookup:
                         t = data[f]
-                
+
                     labels.append(t)
             else:
                 labels.append(e['output']['elements']['text'])
 
             subtitle = []
             if '$' in e['output']['elements']['sub_text']:
-                subtext_lookup = e['output']['elements']['sub_text'].replace("$","").split(".")
+                subtext_lookup = e['output']['elements']['sub_text'].replace(
+                    "$", "").split(".")
                 if isinstance(data, list):
                     for d in data:
                         st = None
@@ -215,7 +230,7 @@ class NLG():
                     st = None
                     for f in subtext_lookup:
                         st = data[f]
-                
+
                     subtitle.append(st)
             else:
                 subtitle.append(e['output']['elements']['text'])
@@ -225,7 +240,8 @@ class NLG():
             if e['output']['elements']['img_embed'] != "":
                 img_embed = e['output']['elements']['img_embed']
             if '$' in e['output']['elements']['image']:
-                img_lookup = e['output']['elements']['image'].replace("$","").split(".")
+                img_lookup = e['output']['elements']['image'].replace(
+                    "$", "").split(".")
                 if isinstance(data, list):
                     for d in data:
                         i = None
@@ -234,7 +250,7 @@ class NLG():
                             i = i_temp[f]
 
                         if img_embed != "":
-                            images.append(img_embed.replace('@',i))
+                            images.append(img_embed.replace('@', i))
                         else:
                             images.append(i)
                 elif isinstance(data[img_lookup[0]], list):
@@ -245,19 +261,18 @@ class NLG():
                             i = i_temp[f]
 
                         if img_embed != "":
-                            images.append(img_embed.replace('@',i))
+                            images.append(img_embed.replace('@', i))
                         else:
                             images.append(i)
                 else:
                     i = None
                     for f in img_lookup:
                         i = data[f]
-                
+
                     images.append(i)
             else:
                 images.append(e['output']['elements']['image'])
 
-            
         buttons = {
             "text": [],
             "payload": []
@@ -266,7 +281,7 @@ class NLG():
         for btn in e['output']['elements']['buttons']:
             btn_text = []
             if '$' in btn['text']:
-                btn_text_lookup = btn['text'].replace("$","").split(".")
+                btn_text_lookup = btn['text'].replace("$", "").split(".")
                 if isinstance(data, list):
                     for d in data:
                         bt = None
@@ -295,19 +310,19 @@ class NLG():
                     btn_text.append(btn['text'])
 
             buttons['text'].append(btn_text)
-            
+
             btn_payload = []
             if '$' in btn['payload']:
-                btn_payload_lookup = btn['payload'].replace("$","").split(".")
+                btn_payload_lookup = btn['payload'].replace("$", "").split(".")
                 if isinstance(data, list):
                     for d in data:
                         bp = None
                         bp_temp = d[btn_payload_lookup[0]]
                         for f in btn_payload_lookup[1:]:
                             bp = bp_temp[f]
-                        
+
                         if btn['embed'] != "":
-                            bp = btn['embed'].replace('@',bp)
+                            bp = btn['embed'].replace('@', bp)
                         btn_payload.append(bp)
                 if isinstance(data[btn_payload_lookup[0]], list):
                     for d in range(len(data[btn_payload_lookup[0]])):
@@ -315,9 +330,9 @@ class NLG():
                         bp_temp = data[btn_payload_lookup[0]][d]
                         for f in btn_payload_lookup[1:]:
                             bp = bp_temp[f]
-                        
+
                         if btn['embed'] != "":
-                            bp = btn['embed'].replace('@',bp)
+                            bp = btn['embed'].replace('@', bp)
                         btn_payload.append(bp)
                 else:
                     bp = None
@@ -325,15 +340,13 @@ class NLG():
                         bp = data[f]
                     for _ in range(len(labels)):
                         if btn['embed'] != "":
-                            bp = btn['embed'].replace('@',bp)
+                            bp = btn['embed'].replace('@', bp)
                         btn_payload.append(bp)
             else:
                 for _ in range(len(labels)):
                     btn_payload.append(btn['payload'])
             buttons['payload'].append(btn_payload)
 
-
-        
         btn_pack = []
         for x in range(len(labels)):
             tmp = []
@@ -344,16 +357,18 @@ class NLG():
                 })
             btn_pack.append(tmp)
         result = {}
-        default_answer = e['output']['elements'].get('default_response_text', '')
-        result["elements"], result['type'] =  self._generate_by_type(e['output']['type'],postbacks,labels,subtitle,images,btn_pack,e['output']['elements']['title'], embed, split_text, default_answer)
+        default_answer = e['output']['elements'].get(
+            'default_response_text', '')
+        result["elements"], result['type'] = self._generate_by_type(
+            e['output']['type'], postbacks, labels, subtitle, images, btn_pack, e['output']['elements']['title'], embed, split_text, default_answer)
         return result
 
     def _generate_by_type(self, type_payload, postbacks, labels, subtitle, images, buttons, title, embed, split_text, default_response):
         if len(labels) > 0:
             if type_payload == self.TYPE_QUICKREPLIES:
                 qr = {
-                "text": self._replace_template_with_value(title)
-                }           
+                    "text": self._replace_template_with_value(title)
+                }
 
                 temp_txt = []
                 for i in range(len(labels)):
@@ -366,7 +381,7 @@ class NLG():
                 return [qr], type_payload
             elif type_payload == self.TYPE_SEARCHBAR:
                 select_box = {
-                    "type":"search_bar",
+                    "type": "search_bar",
                     "text": title,
                     "input_disable": "true"
                 }
@@ -398,23 +413,22 @@ class NLG():
                 if split_text != "":
                     split_text = ","
                 item = split_text.join(labels)
-                text = embed.replace("@",item)
+                text = embed.replace("@", item)
                 return [text], type_payload
         else:
             return [default_response], "text"
 
-from .tracker import Tracker
-from .config import load_config
-from flask import Flask, Response, request
 
 config = load_config()
 response_template = None
 n = NLG(config)
-dashlog = Tracker(config['dashbot']['api'],config['dashbot'][config["template"]["module"]]['api_key'])
+dashlog = Tracker(config['dashbot']['api'], config['dashbot']
+                  [config["template"]["module"]]['api_key'])
 with open('%s' % config["response"]["path"], encoding='utf8') as temp:
     response_template = json.load(temp)
 
 app = Flask(__name__)
+
 
 @app.route("/nlg", methods=['POST'])
 def nlg():
@@ -431,13 +445,12 @@ def nlg():
         lang = slot['language']
     if lang is None or lang == "":
         lang = "en"
-    dashlog.log("incoming", None, sender_id,queryText=text,intent_name=intent)
+    dashlog.log("incoming", None, sender_id,
+                queryText=text, intent_name=intent)
     response = n.get_response(lang)
-    return Response(json.dumps(response,indent=3), mimetype="application/json")
+    return Response(json.dumps(response, indent=3), mimetype="application/json")
 
 
-from waitress import serve
-import os, sys
 env = os.getenv("BLUELOGIC_ENV", "development")
 port = 5006
 if sys.argv[1] == '-p':
