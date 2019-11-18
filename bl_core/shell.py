c48656eaf7fda12779e42dd1e904e81ae8937d48
Rino Alfian <rino@bluelogic.ae>
2019-11-18 12:04:03 +0400

    rasa upgrade and add shell run

---
+++ bl_core/shell.py
@@ -0,0 +1,15 @@
+from rasa.nlu.model import Interpreter
+
+
+def run(lang):
+   interpreter = Interpreter.load('./models/nlu/'+lang+'latest/')
+   print("**type your words**")
+   while True:
+       word = input()
+       print(interpreter.parse(word))
+
+import sys
+lang = "en"
+if sys.argv[1] == '--lang':
+    lang = str(sys.argv[2])
+    run(lang)
