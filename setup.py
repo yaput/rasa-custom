b3266340e5664ac486a7db920d06006c44e4d456
yaput <anton@bluelogic.ae>
2019-12-15 17:07:01 +0400

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bl_core',
-      version='1.4.0',
+      version='1.4.1',
       url='',
       author='Blue Logic',
       author_email='anton@bluelogic.ae',
